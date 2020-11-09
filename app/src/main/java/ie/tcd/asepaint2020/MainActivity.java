4544eed1e4dce4dfb9ae259e65243edc91b6835d
许锐 <xur@2345.com>
2020-11-09 11:08:33 +0000

    add basic fragments

--- app/src/main/java/ie/tcd/asepaint2020/MainActivity.java
+++ app/src/main/java/ie/tcd/asepaint2020/MainActivity.java
@@ -1,7 +1,21 @@
 package ie.tcd.asepaint2020;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import ie.tcd.asepaint2020.event.EnterLobbyEvent;
+import ie.tcd.asepaint2020.event.StartGameEvent;
+import ie.tcd.asepaint2020.fragment.GameFragment;
+import ie.tcd.asepaint2020.fragment.LobbyFragment;
+import ie.tcd.asepaint2020.fragment.NameInputFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EventBus.getDefault().register(this);
+        replaceFragment(new NameInputFragment());
+    }
+
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content, fragment);
+        transaction.commit();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void enterLobby(EnterLobbyEvent event) {
+        // TODO: 09/11/2020  new LobbyFragment should have user name parameters
+        replaceFragment(new LobbyFragment());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void startGame(StartGameEvent event) {
+        // TODO: 09/11/2020  should have game init data
+        replaceFragment(new GameFragment());
     }
 }
---
+++ app/src/main/java/ie/tcd/asepaint2020/fragment/BaseFragment.java
@@ -0,0 +1,25 @@
+package ie.tcd.asepaint2020.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+abstract class BaseFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutId(), container, false);
+        initView(view);
+        return view;
+    }
+
+    abstract int getLayoutId();
+
+    abstract void initView(View view);
+}
